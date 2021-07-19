18f32ef7165b041c93ea334a11292114fd623091
ZoroScripter <87605900+ZoroScripter@users.noreply.github.com>
2021-07-19 18:03:30 -0300

    Of-Sploit
    git clone https://github.com/ZoroScripter/Of-Sploit.git
    cd Of-Sploit
    python3 ofSploit.py

---
+++ ofSploit.py
@@ -0,0 +1,226 @@
+import time
+import os
+
+
+#\033[1;30m Branco
+#\033[1;31m Vermelho
+#\033[1;32m Verde
+#\033[1;33m Amarelo
+#\033[1;34m Azul
+#\033[1;35m Roxo
+#\033[1;36m Ciano
+#\033[1;37m Cinza Claro
+
+os.system("clear")
+os.system("pkg install figlet")
+os.system("clear")
+print("\033[1;36m")
+os.system("figlet Of  Sploit")
+print("""\033[1;33m
+=-=-=-=-=-=-=-=-=-=-=-=-=l
+[1]Somar                 l
+[2]Multiplicar           l
+[3]Install Update        l
+[4]Install Upgrade       l
+[5]Install git           l
+[6]Install Wget          l
+[7]Install python        l
+[8]Install python2       l
+[9]Install Tor           l
+[10]Install Toilet       l
+[11]Cores do python      l
+[12]Install Curl         l
+[13]Install Perl         l
+[14]Install Red Hawk     l
+[15]Install XERXES       l
+[16]Install Ddos-Attack  l
+[17]Install LITESPAM     l
+[18]Install LiteDdos     l
+[19]Install pip          l
+[20]Install php          l
+[21]Install Tbomb        l
+[22]Install Lazy         l
+[23]Install Snyper       l
+[24]Install jdash        l
+[25]Install Kali Linux   l
+[26]Install Libaca       l
+[27]Install Sl           l
+[28]Install LockPhish    l
+[29]Install Consulta CPF l
+[30]Install Kiny-Painel  l
+[31]Install wiki         l
+[32]Creditos             l
+[33]Sair                 l
+=-=-=-=-=-=-=-=-=-=-=-=-=l
+""")
+op = input(">>> ")
+
+if op == "1":
+    n1 = int(imput("Premeiro numero:  "))
+    n2 = int(input("Segundo Numero:   "))
+    resultado = n1+n2
+    print("O Resultado é: {}+{} = {}".format(n1, n2, resultado))
+elif op == "2":
+    n1 = int(input("Premeiro numero: "))
+    n2 = int(input("Segubdo numero: "))
+    produto = n1*n2
+    print("O Resultado é: {}+{} = {}".format(n1, n2, produto))
+    time.sleep(7)
+    os.system("python3 ofSploit.py")
+elif op == "3":
+    os.system("clear")
+    os.system("apt-get update")
+    os.system("python3 ofSploit.py")
+elif op == "4":
+    os.system("clear")
+    os.system("apt-get upgrade")
+    os.system("python3 ofSploit.py")
+elif op == "5":
+    os.system("clear")
+    os.system("pkg install git")
+elif op == "6":
+    os.system("clear")
+    os.system("pkg install wget")
+    os.system("python3 ofSploit.py")
+elif op == "7":
+    os.system("clear")
+    os.system("pkg install python")
+    os.system("python3 ofSploit.py")
+elif op == "8":
+    os.system("clear")
+    os.system("pkg install python2")
+    os.system("python3 ofSploit.py")
+elif op == "9":
+    os.system("clear")
+    os.system("pkg install Tor")
+    os.system("python3 ofSploit.py")
+elif op == "10":
+    os.system("clear")
+    os.system("pkg install toilet")
+    os.system("python3 ofSploit.py")
+elif op == "11":
+    os.system("clear")
+    print("""
+    #\033[1;30m Branco
+    #\033[1;31m Vermelho
+    #\033[1;32m Verde
+    #\033[1;33m Amarelo
+    #\033[1;34m Azul
+    #\033[1;35m Roxo
+    #\033[1;36m Ciano
+    #\033[1;37m Cinza Claro
+    """)
+    time.sleep(7)
+    os.system("python3 ofSploit.py")
+elif op == "12":
+    os.system("clear")
+    os.system("pkg install curl")
+    os.system("python3 ofSploit.py")
+elif op == "13":
+    os.system("clear")
+    os.system("pkg install perl")
+    os.system("python3 ofSploit.py")
+elif op == "14":
+    os.system("clear")
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+    os.system("python3 ofSploit.py")
+elif op == "15":
+  os.system("clear")
+  os.system("git clone https://github.com/XCHADXFAQ77X/XERXES")
+  os.system("python3 ofSploit.py")
+elif op == "16":
+  os.system("clear")
+  os.system("git clone https://github.com/Ha3MrX/DDos-Attack")
+  os.system("python3 ofSploit.py")
+elif op == "17":
+  os.system("clear")
+  os.system("git clone https://github.com/4L13199/LITESPAM")
+  os.system("python3 ofSploit.py")
+elif op == "18":
+  os.system("clear")
+  os.system("git clone https://github.com/4L13199/LITEDDOS")
+  os.system("python3 ofSploit.py")
+elif op == "19":
+  os.system("clear")
+  os.system("pip install requests")
+  os.system("python3 ofSploit.py")
+elif op == "20":
+    os.system("clear")
+    os.system("pkg install php")
+    os.system("python3 ofSploit.py")
+elif op == "21":
+    os.system("clear")
+    os.system("git clone https://github.com/TheSpeedX/TBomb.git")
+    os.system("python3 ofSploit.py")
+elif op == "22":
+    os.system("clear")
+    os.system("git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git")
+    os.system("python3 ofSploit.py")
+elif op == "23":
+    os.system("clear")
+    os.system("git clone https://github.com/1N3/Sn1per")
+    os.system("python3 ofSploit.py")
+elif op == "24":
+    os.system("clear")
+    os.system("git clone https://github.com/sachin175638/jdash")
+    os.system("python3 ofSploit.py")
+elif op == "25":
+    os.system("clear")
+    os.system("git clone https://github.com/noobfoda2/tool")
+    os.system("python3 ofSploit.py")
+elif op == "26":
+    os.system("clear")
+    os.system("pkg install libaca")
+    os.system("python3 ofSploit.py")
+elif op == "27":
+    os.system("clear")
+    os.system("pkg install sl")
+    os.system("python3 ofSploit.py")
+elif op == "28":
+    os.system("clear")
+    os.system("git clone https://github.com/kali-linux-tutorial/lockphis")
+elif op == "29":
+    os.system("clear")
+    os.system("git clone https://github.com/p0isonBR/consultaCPF")
+    os.system("python3 ofSploit.py")
+elif op == "30":
+    os.system("clear")
+    os.system("git clone https://github.com/Kiny-Kiny/Kiny-Painel")
+    os.system("python3 ofSploit.py")
+elif op == "31":
+    os.system("clear")
+    os.system("git clone https://github.com/DeepSociety/wiki-termux")
+    os.system("python3 ofSploit.py")
+elif op == "32":
+    print("""\033[1;33m
+    =-=-=-=-=-=
+    = Author: =
+    =-=-=-=-=-=
+    ZoroScripter
+    
+    ============
+    Apoiadores:
+    ============
+    Columbiaxxx
+    &&
+    W4li
+    
+    =====================
+    =Grupos Apoiadores: =
+    =====================
+    REVOLUTION
+    &&
+    TATAKIO
+    
+    
+    Scripting By
+    Zoro
+    """)
+    os.system("python3 ofSploit.py")
+elif op == "33":
+    os.system("clear")
+    print("Saindo!...")
+    time.sleep(5)
+    exit()
+else:
+    os.system("python3 ofSploit.py")
